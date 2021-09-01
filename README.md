c3feb8b1b162b5bccd4172b8878ec81be23976b0
adam <adaml@gini-apps.com>
2021-09-01 18:53:47 +0300

    edited app.tsx with some basic react type uses

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Getting Started with Create React App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+###cli command:  
+npx create-react-app . --template typescript
+
+note the . specifying that the app should be created in the current directory
 
 ## Available Scripts
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useCallback} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Heading = ({title}: { title: string }) => <h2>{title}</h2>
+// const Box = ({children}: { children: React.ReactNode }) => ( //<--------------------------------------nasty way
+const Box: React.FunctionComponent = ({children}) => ( // nice way
+    <div style={{
+        padding: "1rem",
+        fontWeight: "bold"
+    }}>
+        {children}
     </div>
-  );
+)
+
+const List: React.FunctionComponent<{
+    items: string[];
+    onClick?: (item: string) => void;
+    }> = ({items, onClick}) => (
+    <ul>
+        {items.map((item, idx) => (
+            <li key={idx} onClick={() => onClick?.(item)}>{item}</li>
+        ))}
+    </ul>
+)
+
+function App() {
+    const onListClick = useCallback((item: string) => {
+        console.log(item);
+    }, [])
+
+    return (
+        <div>
+            <Heading title="Introduction"/>
+            <Box>
+                hello there
+            </Box>
+            <List items={['one', 'two', 'three']} onClick={onListClick}/>
+        </div>
+    );
 }
 
 export default App;
